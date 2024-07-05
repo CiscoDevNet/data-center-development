c0b4489939cd36ea814484e951e27c8212c4cfd3
Alexander Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2024-07-05 15:34:46 -0400

    Update config_backup_nx-os.py

--- nx-os/python_scripting_nx-os/on_device/config_backup_nx-os.py
+++ nx-os/python_scripting_nx-os/on_device/config_backup_nx-os.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 from datetime import datetime
 
 def run_cli_command(command):
@@ -15,5 +16,16 @@ def run_cli_command(command):
 if __name__ == "__main__":
     config_output = run_cli_command("show running-config")
     if config_output:
-        with open(f"/bootflash/scripts/config_backup_{datetime.now().strftime('%Y%m%d%H%M%S')}.txt", "w") as config_file:
+        # Create the directory if it doesn't exist
+        logs_dir = "/bootflash/scripts/config-backups"
+        if not os.path.exists(logs_dir):
+            os.makedirs(logs_dir)
+
+        # Define the file path
+        file_path = os.path.join(logs_dir, f"config_backup_{datetime.now().strftime('%Y%m%d%H%M%S')}.txt")
+        
+        # Write the running-config to the file
+        with open(file_path, "w") as config_file:
             config_file.write(config_output)
+
+        print(f"Configuration backup has been written to {file_path}")
